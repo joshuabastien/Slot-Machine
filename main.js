2eae54d0b5dbc91dacc186aaaad79a9736dd3c6c
Joshua Bastien <joshuabastienps@gmail.com>
2023-08-28 01:47:32 -0400

    Added temporary symbol icons and spin button

--- main.js
+++ main.js
@@ -3,10 +3,10 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 var scene = new THREE.Scene();
-var camera = new THREE.OrthographicCamera(window.innerWidth / - 2, window.innerWidth / 2, window.innerHeight / 2, window.innerHeight / - 2, 1, 1000);
+var camera = new THREE.OrthographicCamera(-3, 3, 2, -2, 1, 1000);
 var renderer = new THREE.WebGLRenderer({ alpha: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+document.getElementById('canvas').appendChild(renderer.domElement);
 
 // Add light
 var light = new THREE.PointLight(0xffffff, 1, 1000);
@@ -20,6 +20,26 @@ controls.enableDamping = true;
 controls.dampingFactor = 0.25;
 controls.screenSpacePanning = false;
 controls.maxPolarAngle = Math.PI / 2;
+
+// Load textures
+var textureLoader = new THREE.TextureLoader();
+var textures = [];
+for (var i = 1; i <= 8; i++) {
+    textures.push(textureLoader.load('assets/icon' + i + '.png'));
+}
+
+// Create meshes and add to scene
+var meshes = [];
+for (var i = 0; i < 5; i++) {
+    for (var j = 0; j < 3; j++) {
+        var material = new THREE.MeshBasicMaterial({ map: textures[(i * 3 + j) % 8] });
+        var geometry = new THREE.PlaneGeometry(1, 1);
+        var mesh = new THREE.Mesh(geometry, material);
+        mesh.position.set(i - 2, 1 - j, 0);
+        scene.add(mesh);
+        meshes.push(mesh);
+    }
+}
 
 // Render the scene
 function animate() {
