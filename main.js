d34dc442f1b8b03bca882403a04d8a2b85b40f93
Joshua Bastien <joshuabastienps@gmail.com>
2023-08-31 00:17:54 -0400

    Added assets

--- main.js
+++ main.js
@@ -13,10 +13,10 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.getElementById('canvas').appendChild(renderer.domElement);
 
 // Add light
-var light = new THREE.PointLight(0xffffff, 1, 1000);
+var light = new THREE.PointLight(0xffffff, 2, 1000);
 light.position.set(0, 0, 10);
 scene.add(light);
-var ambientLight = new THREE.AmbientLight(0xffffff); // full intensity
+var ambientLight = new THREE.AmbientLight(0xffffff, 2); // full intensity
 scene.add(ambientLight);
 
 camera.position.z = 100; 
@@ -40,33 +40,49 @@ for (var i = 1; i <= 8; i++) {
     textures.push(textureLoader.load('assets/icon' + i + '.png'));
 }
 
+// var borderMaterial = new THREE.MeshBasicMaterial({ color: 0x000000 }); // black color
+
 // Create meshes and add to scene
 var meshes = [];
 for (var i = 0; i < 5; i++) {
     for (var j = 0; j < 3; j++) {
-        var material = new THREE.MeshBasicMaterial({ map: textures[(i * 3 + j) % 8] });
-        var geometry = new THREE.BoxGeometry(1, 1, 1); // Change this line
+        var material = new THREE.MeshStandardMaterial({ map: textures[(i * 3 + j) % 8], transparent: true, alphaTest: 0.5 });
+
+        // Adjust the size of the geometry
+        var geometry = new THREE.PlaneGeometry(1.55, 1.55); // Change from (2, 2) to (1.5, 1.5)
+        
         var mesh = new THREE.Mesh(geometry, material);
-        mesh.position.set(i - 2, 1 - j, 0);
+        
+        // Adjust the position accordingly
+        mesh.position.set((i - 2) * 1.5, (1 - j) * 1.5, 0);
+        
         scene.add(mesh);
         meshes.push(mesh);
     }
+    
+    // add borders between the columns of reels
+    // if (i < 4) {
+    //     var borderGeometry = new THREE.BoxGeometry(0.1, 3, 1); 
+    //     var borderMesh = new THREE.Mesh(borderGeometry, borderMaterial);
+    //     borderMesh.position.set(i - 1.45, 0, 0);
+    //     scene.add(borderMesh);
+    // }
 }
 
 const gltfLoader = new GLTFLoader();
 
-gltfLoader.load('/assets/Warrior of the Ocean/scene.gltf', (gltf) => {
-    gltf.scene.position.set(4.5, -1, 0); //(left/right, up/down, forward/backward)
-    gltf.scene.scale.set(10, 10, 10);
+gltfLoader.load('/assets/shark warrior/scene.gltf', (gltf) => {
+    gltf.scene.position.set(5, 2, -1); //(left/right, up/down, forward/backward)
+    gltf.scene.scale.set(0.5, 0.5, 0.5);
     scene.add(gltf.scene);
 });
 
-gltfLoader.load('/assets/Ruin Scene/scene.gltf', (gltf) => {
-    gltf.scene.scale.set(8, 8, 8);
-    gltf.scene.position.set(3, -5.5, -2); //(left/right, up/down, forward/backward)
-    gltf.scene.rotation.y = Math.PI / 2;
-    scene.add(gltf.scene);
-});
+// gltfLoader.load('/assets/Ruin Scene/scene.gltf', (gltf) => {
+//     gltf.scene.scale.set(8, 8, 8);
+//     gltf.scene.position.set(2.5, -6, -2); //(left/right, up/down, forward/backward)
+//     gltf.scene.rotation.y = Math.PI / 2;
+//     scene.add(gltf.scene);
+// });
 
 var mixer;
 var fish;
@@ -76,7 +92,7 @@ var speed = 0.01;
 gltfLoader.load('/assets/Ugly Fish/scene.gltf', (gltf) => {
     fish = gltf.scene;
     fish.scale.set(0.01, 0.01, 0.01);
-    fish.position.set(-10, 1, 2);
+    fish.position.set(-10, 2, 2);
     fish.rotation.y = Math.PI / 2;
     scene.add(fish);
 
